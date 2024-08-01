fc7a9aa629d745eb44f9d5edce421a6d977b13f6
itsBlairW <71138822+Jingyiii-W@users.noreply.github.com>
2024-08-01 16:14:20 -0400

    fix erroes

--- blog.config.js
+++ blog.config.js
@@ -20,13 +20,13 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
   process.env.NEXT_PUBLIC_GREETING_WORDS ||
-  "Hi It's Blair, Welcome to my Blog🥰",
+  "Hi It's Blair, Welcome to my Universe🥰",
 
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Blair Wang', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || "🇨🇳 ➡️ 🇨🇦 🎓 Bsc | Kingston | QU | 23' 🎓 MEng | London | UWO | 24' 🔧 Software engineer | Machine Learning Engineer | Data Engineer", // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || "Love& Live", // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://itsblair.vercel.app/en', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blair, Blog', // 网站关键词 英文逗号隔开
 
