10fdf4c2d16c1fc4ed16881be8e3289a355eec08
itsBlairW <71138822+Jingyiii-W@users.noreply.github.com>
2024-08-02 03:35:55 -0400

    update website link

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Blair Wang', // 您的昵称 
   BIO: process.env.NEXT_PUBLIC_BIO || "Love& Live", // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://itsblair.vercel.app/en', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'blairwang.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Blair, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
