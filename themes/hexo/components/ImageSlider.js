5e1f64c634699338131b6f3a71392cf8121152de
itsBlairW <71138822+Jingyiii-W@users.noreply.github.com>
2024-08-02 03:19:17 -0400

    haha

--- themes/hexo/components/ImageSlider.js
+++ themes/hexo/components/ImageSlider.js
@@ -3,36 +3,36 @@ import React, { useState, useEffect } from 'react';
 
 const ImageSlider = () => {
     const images = [
-      // {
-      //   src: '/images/image1.jpg',
-      //   alt: 'Image 1',
-      //   placeholderSrc: '/images/placeholder.JPG' // 这里可以放占位图的路径
-      // },
-      // {
-      //   src: '/images/image2.jpg',
-      //   alt: 'Image 2',
-      //   placeholderSrc: '/images/placeholder.JPG'
-      // },
-      // {
-      //   src: '/images/image3.jpg',
-      //   alt: 'Image 3',
-      //   placeholderSrc: '/images/placeholder.JPG'
-      // },
-      // {
-      //   src: '/images/image4.jpg',
-      //   alt: 'Image 4',
-      //   placeholderSrc: '/images/placeholder.JPG'
-      // },
-      // {
-      //   src: '/images/image5.jpg',
-      //   alt: 'Image 5',
-      //   placeholderSrc: '/images/placeholder.JPG'
-      // },
-      // {
-      //   src: '/images/image6.jpg',
-      //   alt: 'Image 6',
-      //   placeholderSrc: '/images/placeholder.JPG'
-      // }
+      {
+        src: '/images/image1.jpg',
+        alt: 'Image 1',
+        placeholderSrc: '/images/placeholder.JPG' // 这里可以放占位图的路径
+      },
+      {
+        src: '/images/image2.jpg',
+        alt: 'Image 2',
+        placeholderSrc: '/images/placeholder.JPG'
+      },
+      {
+        src: '/images/image3.jpg',
+        alt: 'Image 3',
+        placeholderSrc: '/images/placeholder.JPG'
+      },
+      {
+        src: '/images/image4.jpg',
+        alt: 'Image 4',
+        placeholderSrc: '/images/placeholder.JPG'
+      },
+      {
+        src: '/images/image5.jpg',
+        alt: 'Image 5',
+        placeholderSrc: '/images/placeholder.JPG'
+      },
+      {
+        src: '/images/image6.jpg',
+        alt: 'Image 6',
+        placeholderSrc: '/images/placeholder.JPG'
+      }
       //添加更多图片对象
     ];
 
@@ -72,7 +72,7 @@ const ImageSlider = () => {
         <button onClick={prevSlide}>❮</button>
         <div className='slider'>
           <div className='slider-image-container relative overflow-hidden w-80 h-60'> {/* 设置固定大小 */}
-          <img
+          {/* <img
               src={images[currentIndex].src}
               alt={images[currentIndex].alt}
               placeholderSrc={images[currentIndex].placeholderSrc}
@@ -80,7 +80,7 @@ const ImageSlider = () => {
               width={800} // 可以根据需要设置宽度
               height={600} // 可以根据需要设置高度
               onLoad={() => console.log('Image loaded!')} // 可选的加载回调
-            />
+            /> */}
           </div>
         </div>
         <button onClick={nextSlide}>❯</button>
